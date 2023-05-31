75d7ddc09f4e7c60f894ac70834f052a3143b812
Prashant <prashant@sumfactor.com>
2023-05-31 23:39:24 +0530

    start section-14

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 3. set-up fonts
 4. Add font to docuemnt
 5. Global app styling
+
+## Section-14 :
+
+1. Netflix Component Architecture
+2. Banner component
+3. Banner component structure
+4. Banner component Button with font
+5. Icon with google font
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Banner from '../components/banner';
 
-export default function Home(){
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Netflix</title>
-        <link rel='icon' href='/favicon.ico'/>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <h1>Netflix</h1>
+
+      {/* <NavBar/> */}
+      <Banner 
+      title='Clifford the red dog' 
+      subTitle='a very cute dog' 
+      imgUrl='clifford.webp' />
     </div>
+
+
   )
 }
